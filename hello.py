2268b627325795b9db73ff11a3bf7cea1b4d1552
nattakits <40267160+nattakits@users.noreply.github.com>
2021-03-21 13:47:47 +0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ from flask import Flask
 from flask_jsonpify import jsonpify
 from flask_restful import Resource,Api,reqparse
 import requests
-import pandas as pd
+
 
 app = Flask(__name__)
 api=Api(app)
