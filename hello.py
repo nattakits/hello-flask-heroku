22df9b16f907701422c47d89dbbc40235dfc9ed4
nattakits <40267160+nattakits@users.noreply.github.com>
2021-03-21 14:16:03 +0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,10 +1,8 @@
 from flask import Flask
 import json
-import time
 from flask_jsonpify import jsonpify
 from flask_restful import Resource,Api,reqparse
 import requests
-import pandas as pd
 app = Flask(__name__)
 
 @app.route('/')
