978060fa3432d519d28d44270de5ec52f3113023
nattakits <40267160+nattakits@users.noreply.github.com>
2021-03-21 13:38:37 +0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,9 +1,40 @@
+import json
+import time
 from flask import Flask
-app = Flask(__name__)
+from flask_jsonpify import jsonpify
+from flask_restful import Resource,Api,reqparse
+import requests
+import pandas as pd
 
+app = Flask(__name__)
+api=Api(app)
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+class get_mathcal(Resource):
+    def get(self):
+        parser=reqparse.RequestParser()
+        parser.add_argument('input1',type=str)
+        parser.add_argument('input2',type=str)
+        parser.add_argument('operation',type=str)
+        dictp=parser.parse_args()
+        inp1=dictp['input1']
+        inp2=dictp['input2']
+        oper=dictp['operation']
+        if(oper=='plus'):
+            oper='+'
+        else:
+            oper='-'
+        stringcal=inp1+' '+oper+' '+inp2
+        print(stringcal)
+        answ = eval(stringcal)
+        result={}
+        result['stringcal']=stringcal
+        result['answer']=answ
+        return(result)
+api.add_resource(get_mathcal, '/cal',endpoint='cal')
+
 
 if __name__ == '__main__':
     app.run()
