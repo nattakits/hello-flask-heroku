9fcb9a98a0e50bcd232b46406a4f1601500fc2f1
nattakits <40267160+nattakits@users.noreply.github.com>
2021-03-21 13:59:07 +0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,16 +1,9 @@
-import json
 from flask import Flask
-from flask_jsonpify import jsonpify
-from flask_restful import Resource,Api,reqparse
-import requests
-import pandas as pd
-
 app = Flask(__name__)
-api=Api(app)
+
 @app.route('/')
 def hello_world():
     return 'Hello World!'
-
 
 if __name__ == '__main__':
     app.run()
